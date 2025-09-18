162ddae03f81d5b2a9d42e1555690a9def69b463
madhav <madhavendranaths@gmail.com>
2025-09-18 16:46:48 +0530

    auth completed

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,13 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export const meta=()=>(
+    [{title:"Resume Analyser | auth"},
+     {name:"description" ,content:"log into your account"}
+    ]
+)
+
+export default 
+[   index("routes/home.tsx"),
+    route("/auth","routes/auth.tsx")
+
+] satisfies RouteConfig;

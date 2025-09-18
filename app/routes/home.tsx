405139872ff9ff546b2c691c676ae2580aec873f
madhav <madhavendranaths@gmail.com>
2025-09-18 15:30:43 +0530

    setup basic ui moving to auth

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
+import { resumes } from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,13 +15,20 @@ export default function Home() {
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar></Navbar>
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>
             Track Your Applications And Resume Ratings
           </h1>
 
         </div>
       </section>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume)=>(
+            <ResumeCard key={resume.id} resume={resume}></ResumeCard>
+          ))}
+        </div>
+      )}
     </main>
   ) 
 }

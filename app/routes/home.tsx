28b8ae8d019b4ae8e6affba795db74c60bc39958
madhav <madhavendranaths@gmail.com>
2025-09-18 14:49:43 +0530

    UI implemented

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,25 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyser" },
+    { name: "description", content: "New way to anlayse your Resume!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return(
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar></Navbar>
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>
+            Track Your Applications And Resume Ratings
+          </h1>
+
+        </div>
+      </section>
+    </main>
+  ) 
 }
